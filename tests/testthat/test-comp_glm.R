e9b02f9678b35049fc0ce0f7071d462364da411f
jameswolpe <jjwolp19@stlawu.edu>
2023-11-16 09:31:49 -0500

    Updated tests and made new vignette

--- tests/testthat/test-comp_glm.R
+++ tests/testthat/test-comp_glm.R
@@ -4,3 +4,18 @@ test_that("model fits", {
                            p1_effects = ~ 1, p2_effects = ~ 1,
                            ref_player = "Milos Raonic"))
 })
+
+test_that("snapshot test for comp_glm", {
+  comp_glm_result <- comp_glm(point_winner ~ -1, data = tennis_point,
+                              p1 = "player1", p2 = "player2",
+                              p1_effects = ~ 1, p2_effects = ~ 1,
+                              ref_player = "Milos Raonic")
+  expect_snapshot(
+    waldo::compare(
+      cat(comp_glm_result)
+    )
+  )
+})
+
+# tried expect_snapshot_output
+#
