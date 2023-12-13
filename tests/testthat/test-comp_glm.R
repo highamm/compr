b6a24dbd5c28051869328b3d8448345b3e46691d
jameswolpe <jjwolp19@stlawu.edu>
2023-12-13 14:15:36 -0500

    added ref_player not in data set warning test

--- tests/testthat/test-comp_glm.R
+++ tests/testthat/test-comp_glm.R
@@ -25,3 +25,15 @@ test_that("snapshot test for model with covariates", {
     comp_glm_cov_result
   )
 })
+
+test_that("expect warning if ref_player not in data set", {
+  expect_warning({
+    comp_glm(point_winner ~ -1, data = tennis_point,
+             p1 = "player1", p2 = "player2",
+             p1_effects = ~ 1, p2_effects = ~ 1,
+             ref_player = "Nonexistent Player")
+  }, "Reference player 'Nonexistent Player' not found in the dataset.")
+})
+
+# aug_mod testing
+#TODO might have to do an augment test does it throw an error if the player is not in the data set
